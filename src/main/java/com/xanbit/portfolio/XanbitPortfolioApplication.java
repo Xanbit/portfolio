b4bf604bad1c94c2123ac7e2644e5ca360269563
markiv <markiv@macbook1s-MacBook-Air.local>
2016-05-28 13:55:56 +0530

    launch date information added in landing page

--- src/main/java/com/xanbit/portfolio/XanbitPortfolioApplication.java
+++ src/main/java/com/xanbit/portfolio/XanbitPortfolioApplication.java
@@ -1,15 +1,17 @@
 package com.xanbit.portfolio;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +33,23 @@ class HomeController {
       Map<String,Object> model = new HashMap<String,Object>();
       model.put("id", UUID.randomUUID().toString());
       model.put("content", "Under Construction....");
-      model.put("launchdate", new Date());
+      Date launchDate = getLaunchDate();
+      model.put("launchdate", launchDate != null ? launchDate : new Date());
       return model;
+    }
+    
+    private Date getLaunchDate(){
+
+        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        isoFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+        
+        try {
+			Date date = isoFormat.parse("2016-06-06T09:00:00");
+			return date;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+        return null;
     }
 }
 
--- src/main/resources/static/js/home.ctrl.js
+++ src/main/resources/static/js/home.ctrl.js
@@ -1,5 +1,6 @@
 angular.module('xanbitApp').controller('home', function($scope, $http) {
   $http.get('/info/').success(function(data) {
     $scope.info = data;
+    $scope.launchdate = new Date(data.launchdate).toUTCString();
   })
 });
